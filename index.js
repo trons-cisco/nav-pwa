fd2ee05d1e6028a5c1fefd1710a9d7d9ddf03840
trons-cisco <124304505+trons-cisco@users.noreply.github.com>
2023-02-05 22:40:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function setledcontrolmanual(){
 	xapi.Config.UserInterface.LedControl.Mode.set('Manual');
 };
 
+setledcontrolmanual();
+
 //Initialize the xAPI on page load to make the xapi object available.
 //Creating a persistent Cookie with navigator ID is not necessary, 
 //just done as an example for how to distinguish between unique navigators.
